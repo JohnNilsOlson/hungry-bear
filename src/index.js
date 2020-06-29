2ac440a88944e75d04e0815ffc32adff6d8d0768
JohnNils Olson <johnnils@gmail.com>
2020-06-29 10:02:21 -0700

    add content to index.html, index.js, hungrybear.js and tests
    Co-Authored-By: Megan Hepner <megan.hepner@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,3 +2,22 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+import { HungryBear } from './../src/hungrybear.js';
+
+$(document).ready(function() {
+
+  let fuzzy;
+
+  $("#start").submit(function(event) {
+    event.preventDefault();
+    let fuzzy = new HungryBear;
+    fuzzy.setHunger();
+    $("#hunger-level").text(fuzzy.foodLevel);
+  });
+
+  $("#feed").click(function() {
+    fuzzy.feed();
+  });
+
+
+});
